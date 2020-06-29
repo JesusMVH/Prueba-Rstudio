64adf42edee0abc337e09a85b46dc2f27b4b60c5
J.M Valencia <marcos.valencia.99@outlook.com>
2020-06-29 14:22:12 -0600

    test 3

--- git_1.R
+++ git_1.R
@@ -1,9 +1,7 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
-<<<<<<< HEAD
-print ("Hola soy de R studio")
-Hprint ("hola")
-=======
-Hola estoy testeando
->>>>>>> c6c9ab8001c3bb9e8be2a2d258ff6ed8e87f2b01
+
+
+hola
+print ("no entiendo")
